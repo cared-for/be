1fbc5802f85e453f6e4b23a9e562bd8ab7cf6cd3
Stanley Szeto <gw.stanley.szeto@gmail.com>
2024-01-28 17:38:21 +0900

    testing gather endpoint

--- index.ts
+++ index.ts
@@ -47,27 +47,26 @@ const voice = async (req: Request) => {
 const gather = async (req: Request) => {
   // Use the Twilio Node.js SDK to build an XML response
   const twiml = new VoiceResponse();
-  const body = await req.json();
-
-  console.log(body.Digits);
-
-  // If the user entered digits, process their request
-  if (body.Digits) {
-    switch (body.Digits) {
-      case '1':
-        twiml.say('Thanks for checking in! Have a great day!');
-        break;
-      default:
-        twiml.say("Sorry, look like you picked a different number");
-        twiml.pause();
-        twiml.redirect(`${process.env.HOST}/voice`);
-        break;
-    }
-  } else {
-    // If no input was sent, redirect to the /voice route
-    twiml.redirect(`${process.env.HOST}/voice`);
-  }
-
+  const body = req.text();
+  console.log("body: ", body);
+  //
+  // // If the user entered digits, process their request
+  // if (req.body.Digits) {
+  //   switch (req.body!.Digits) {
+  //     case '1':
+  //       twiml.say('Thanks for checking in! Have a great day!');
+  //       break;
+  //     default:
+  //       twiml.say("Sorry, look like you picked a different number");
+  //       twiml.pause();
+  //       twiml.redirect(`${process.env.HOST}/voice`);
+  //       break;
+  //   }
+  // } else {
+  //   // If no input was sent, redirect to the /voice route
+  //   twiml.redirect(`${process.env.HOST}/voice`);
+  // }
+  //
   // Render the response as XML in reply to the webhook request
   return new Response(twiml.toString(), {
     headers: { 'Content-Type': 'text/xml' },
