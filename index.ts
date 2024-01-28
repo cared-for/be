a88831ced1fdd61197e074d7f0746a0e28dc62d2
Stanley Szeto <gw.stanley.szeto@gmail.com>
2024-01-28 17:55:31 +0900

    test

--- index.ts
+++ index.ts
@@ -1,10 +1,24 @@
 import Twilio from "twilio";
+import queryString from 'query-string';
 import "dotenv/config";
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = Twilio(accountSid, authToken);
 const VoiceResponse = Twilio.twiml.VoiceResponse;
+
+const outboundCall = async () => {
+  await client.calls.create({
+    method: "POST",
+    url: `${process.env.HOST}/voice`,
+    to: "+16195677998",
+    from: "+13239828587",
+  })
+
+  return new Response("Success!", {
+    headers: { "content-type": "text/xml" },
+  });
+}
 
 // Create a route that will handle Twilio webhook requests, sent as an
 // HTTP POST to /voice in our application
@@ -23,53 +37,37 @@ const voice = async (req: Request) => {
 
   // If the user doesn't enter input, loop
   twiml.redirect(`${process.env.HOST}/voice`);
-  
-  const twimlCommand = twiml.toString();
-  console.log(twiml.toString());
-
-  await client.calls.create({
-    twiml: twimlCommand,
-    to: "+16195677998",
-    from: "+13239828587",
-  })
-
-  return new Response("Success!", {
-    headers: { "content-type": "text/xml" },
-  })
 
   // Render the response as XML in reply to the webhook request
-  // return new Response(twiml.toString(), {
-  //   headers: { 'Content-Type': 'text/xml' },
-  // });
+  return new Response(twiml.toString(), {
+    headers: { 'Content-Type': 'text/xml' },
+  });
 };
-
-import queryString from 'query-string';
 
 // Create a route that will handle <Gather> input
 const gather = async (req: Request) => {
   // Use the Twilio Node.js SDK to build an XML response
   const twiml = new VoiceResponse();
   const body = await req.text();
-  console.log("does digits exist?: ", body.includes("Digits"));
-  console.log("body: ", queryString.parse(body));
+  const params = queryString.parse(body);
 
-  // // If the user entered digits, process their request
-  // if (req.body.Digits) {
-  //   switch (req.body!.Digits) {
-  //     case '1':
-  //       twiml.say('Thanks for checking in! Have a great day!');
-  //       break;
-  //     default:
-  //       twiml.say("Sorry, look like you picked a different number");
-  //       twiml.pause();
-  //       twiml.redirect(`${process.env.HOST}/voice`);
-  //       break;
-  //   }
-  // } else {
-  //   // If no input was sent, redirect to the /voice route
-  //   twiml.redirect(`${process.env.HOST}/voice`);
-  // }
-  //
+  // If the user entered digits, process their request
+  if (params.Digits) {
+    switch (params.Digits) {
+      case '1':
+        twiml.say('Thanks for checking in! Have a great day!');
+        break;
+      default:
+        twiml.say("Sorry, look like you picked a different number");
+        twiml.pause();
+        twiml.redirect(`${process.env.HOST}/voice`);
+        break;
+    }
+  } else {
+    // If no input was sent, redirect to the /voice route
+    twiml.redirect(`${process.env.HOST}/voice`);
+  }
+
   // Render the response as XML in reply to the webhook request
   return new Response(twiml.toString(), {
     headers: { 'Content-Type': 'text/xml' },
